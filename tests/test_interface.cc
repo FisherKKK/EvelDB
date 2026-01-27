7bf066621f3c02d84413d656bb9770ed0ee96fb0
FisherKK <1658654007@qq.com>
2026-01-27 23:51:56 +0000

    init project

--- tests/test_interface.cc
+++
@@ -1,8 +0,0 @@
-#include "eveldb/db.h"
-
-int main() {
-  eveldb::DB* db;
-
-  auto status = eveldb::DB::Open(eveldb::Options(), "/tmp/test-0", &db);
-  return 0;
-}
--- util/options.cc
+++
@@ -1,7 +0,0 @@
-#include "eveldb/options.h"
-
-#include "eveldb/env.h"
-
-namespace eveldb {
-Options::Options() : env(Env::Default()) {}
-}  // namespace eveldb
